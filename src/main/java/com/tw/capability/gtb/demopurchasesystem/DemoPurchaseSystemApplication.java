41d1882546f3ad7e1b3da3e4e364ef2f0231d729
SullyW <18921310798@163.com>
2023-02-07 20:50:50 +0800

    feat: generate project skeleton

---
+++ src/main/java/com/tw/capability/gtb/demopurchasesystem/DemoPurchaseSystemApplication.java
@@ -0,0 +1,13 @@
+package com.tw.capability.gtb.demopurchasesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoPurchaseSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoPurchaseSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tw/capability/gtb/demopurchasesystem/DemoPurchaseSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tw.capability.gtb.demopurchasesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoPurchaseSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
