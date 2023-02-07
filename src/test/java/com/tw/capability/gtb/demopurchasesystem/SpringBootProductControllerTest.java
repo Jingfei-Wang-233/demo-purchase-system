6df0dc7dc87aa55844a4ba40844bc1c8c65d047c
SullyW <18921310798@163.com>
2023-02-08 01:01:50 +0800

    refactor: use sql annotation for test data

--- src/test/java/com/tw/capability/gtb/demopurchasesystem/SpringBootProductControllerTest.java
+++ src/test/java/com/tw/capability/gtb/demopurchasesystem/SpringBootProductControllerTest.java
@@ -1,7 +1,6 @@
 package com.tw.capability.gtb.demopurchasesystem;
 
 import com.tw.capability.gtb.demopurchasesystem.domain.Product;
-import com.tw.capability.gtb.demopurchasesystem.repository.ProductRepository;
 import com.tw.capability.gtb.demopurchasesystem.web.ProductController;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
@@ -12,12 +11,14 @@ import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.jdbc.Sql;
 
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureJsonTesters
@@ -29,8 +30,6 @@ class SpringBootProductControllerTest {
     private JacksonTester<List<Product>> productsJson;
     @Autowired
     private ProductController productController;
-    @Autowired
-    private ProductRepository productRepository;
 
     @AfterEach
     void tearDown() {
@@ -49,13 +48,9 @@ class SpringBootProductControllerTest {
     }
 
     @Test
+    @Sql("/sql/insert_3_products.sql")
     void should_return_all_products() throws IOException {
         // Given
-        final var products = List.of(
-                new Product("product 01", BigDecimal.valueOf(100.00), "basic management software", "Windows", 1),
-                new Product("product 02", BigDecimal.valueOf(200.00), "advanced management software", "Windows", 2),
-                new Product("product 03", BigDecimal.valueOf(300.00), "ultimate management software", "Windows", 3));
-        productRepository.saveAll(products);
         // When
         final var responseEntity = restTemplate.getForEntity("/products", String.class);
         // Then
@@ -63,5 +58,6 @@ class SpringBootProductControllerTest {
         assertThat(responseEntity.getHeaders().getContentType()).isEqualTo(MediaType.APPLICATION_JSON);
         final var fetchedProducts = responseEntity.getBody();
         assertThat(productsJson.parseObject(fetchedProducts)).hasSize(3);
+        assertEquals(0, BigDecimal.valueOf(100).compareTo(productsJson.parseObject(fetchedProducts).get(0).getPrice()));
     }
 }
---
+++ src/test/resources/sql/insert_3_products.sql
@@ -0,0 +1,3 @@
+insert into products(id,description, platform, price, tier, title) values (1,'basic product','windows',100,1,'p1');
+insert into products(id,description, platform, price, tier, title) values (2,'advanced product','windows',200,2,'p2');
+insert into products(id,description, platform, price, tier, title) values (3,'ultimate product','windows',300,3,'p3');
