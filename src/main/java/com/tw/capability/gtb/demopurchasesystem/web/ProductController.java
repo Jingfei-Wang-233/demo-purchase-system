dea45f7e2a5a49d46078f1900844b09df40cdccd
SullyW <18921310798@163.com>
2023-02-08 21:26:47 +0800

    refactor: extract constants

--- src/main/java/com/tw/capability/gtb/demopurchasesystem/web/ProductController.java
+++ src/main/java/com/tw/capability/gtb/demopurchasesystem/web/ProductController.java
@@ -4,7 +4,10 @@ import com.tw.capability.gtb.demopurchasesystem.service.ProductService;
 import com.tw.capability.gtb.demopurchasesystem.web.dto.response.PageResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
@@ -13,11 +16,13 @@ import org.springframework.web.bind.annotation.*;
 public class ProductController {
 
 
+    public static final String DEFAULT_PAGE_NUMBER = "1";
+    public static final String DEFAULT_PAGE_SIZE = "10";
     private final ProductService productService;
 
     @GetMapping
-    public ResponseEntity<PageResponse> fetchAllInPaging(@RequestParam(required = false, defaultValue = "1") int pageNumber,
-                                                         @RequestParam(required = false, defaultValue = "10") int pageSize) {
+    public ResponseEntity<PageResponse> fetchAllInPaging(@RequestParam(required = false, defaultValue = DEFAULT_PAGE_NUMBER) int pageNumber,
+                                                         @RequestParam(required = false, defaultValue = DEFAULT_PAGE_SIZE) int pageSize) {
         PageResponse pageResponse = productService.fetchAllInPaging(pageNumber - 1, pageSize);
         pageResponse.setNumber(pageResponse.getNumber() + 1);
         return ResponseEntity.ok().body(pageResponse);
